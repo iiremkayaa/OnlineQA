64599e893e0509f92cf308e17e72c361c0b1ab33
İrem Kaya <38073758+iiremkayaa@users.noreply.github.com>
2020-10-09 20:20:49 +0300

    added controllers

---
+++ Project.OnlineQA.WebApi/Controllers/SelectionsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Project.OnlineQA.Business.Interface;
+
+namespace Project.OnlineQA.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SelectionsController : ControllerBase
+    {
+        private readonly ISelectionService _selectionService;
+        public SelectionsController(ISelectionService selectionService)
+        {
+            _selectionService = selectionService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _selectionService.GetAllAsync());
+        }
+    }
+}
---
+++ Project.OnlineQA.WebApi/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Project.OnlineQA.Business.Interface;
+
+namespace Project.OnlineQA.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _userService.GetAllAsync());
+        }
+    }
+}
